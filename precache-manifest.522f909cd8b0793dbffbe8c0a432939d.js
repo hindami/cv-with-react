cc442acb4dbc3a87908470cc143b51d6e5d43fd9
hindami <hindami050294@gmail.com>
2020-03-04 16:14:12 +0700

    Updates

---
+++ precache-manifest.522f909cd8b0793dbffbe8c0a432939d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2753adac64730d862b763cc7dd244cb0",
+    "url": "/cv-with-react/index.html"
+  },
+  {
+    "revision": "6178c8867a5679787d6b",
+    "url": "/cv-with-react/static/css/main.8d29b36b.chunk.css"
+  },
+  {
+    "revision": "825cc0b43e156403d183",
+    "url": "/cv-with-react/static/js/2.e12af6fb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cv-with-react/static/js/2.e12af6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6178c8867a5679787d6b",
+    "url": "/cv-with-react/static/js/main.11655198.chunk.js"
+  },
+  {
+    "revision": "3a207aeade7652ceb0da",
+    "url": "/cv-with-react/static/js/runtime-main.a4777e73.js"
+  },
+  {
+    "revision": "0d8d233464e549560286b7402be0b4c6",
+    "url": "/cv-with-react/static/media/bootstrap(color).0d8d2334.svg"
+  },
+  {
+    "revision": "bf33167dd3b3e9712937efede5d22e84",
+    "url": "/cv-with-react/static/media/briefcase(white).bf33167d.svg"
+  },
+  {
+    "revision": "86795cf20943d8a1d24cf471fa6273ae",
+    "url": "/cv-with-react/static/media/cogs(white).86795cf2.svg"
+  },
+  {
+    "revision": "e6c01aecdbd5d7f73c6e0cfaae1a8305",
+    "url": "/cv-with-react/static/media/css(blue).e6c01aec.svg"
+  },
+  {
+    "revision": "f4a8d890ba0e505640e4d0578eb693d5",
+    "url": "/cv-with-react/static/media/fb(white).f4a8d890.svg"
+  },
+  {
+    "revision": "7bb69c5dac878ad3d0d8574c2afcda75",
+    "url": "/cv-with-react/static/media/github(white).7bb69c5d.svg"
+  },
+  {
+    "revision": "064bfdbaeba315dd5771f4996b13d011",
+    "url": "/cv-with-react/static/media/graduation-cap(white).064bfdba.svg"
+  },
+  {
+    "revision": "056f4632bc40f87bcfcb5a0c8e03768a",
+    "url": "/cv-with-react/static/media/html5(orange).056f4632.svg"
+  },
+  {
+    "revision": "a806fe208207e3a86b231d4ff036e548",
+    "url": "/cv-with-react/static/media/instagram(white).a806fe20.svg"
+  },
+  {
+    "revision": "f29eb9288543f3029decabfc71145952",
+    "url": "/cv-with-react/static/media/js(yellow).f29eb928.svg"
+  },
+  {
+    "revision": "143003bbaf70a6ff280e078bc148932b",
+    "url": "/cv-with-react/static/media/pasFoto.143003bb.jpg"
+  },
+  {
+    "revision": "0755ab742d9f8d1f6c975153d39a1e91",
+    "url": "/cv-with-react/static/media/react(blue).0755ab74.svg"
+  }
+]);
